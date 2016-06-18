fc043173ce067c0006b76131c6af3e6d5c5f3765
Alex May <bigfug@gmail.com>
2016-06-18 10:27:04 +0100

    Updated IndexNode with auto-rename and also fixed up its implementation, which had obviously degraded a bit over time!

--- plugins/Core/indexnode.h
+++ plugins/Core/indexnode.h
@@ -20,18 +20,22 @@ public:
 	virtual ~IndexNode( void ) {}
 
 	// NodeControlInterface interface
+public:
+	virtual void inputsUpdated( qint64 pTimeStamp ) Q_DECL_OVERRIDE;
 
-	virtual void inputsUpdated(qint64 pTimeStamp);
+	virtual QList<QUuid> pinAddTypesInput() const Q_DECL_OVERRIDE;
 
-	virtual QList<QUuid> pinAddTypesInput() const;
+	virtual bool canAcceptPin( fugio::PinInterface *pPin ) const Q_DECL_OVERRIDE;
 
-	virtual bool canAcceptPin( fugio::PinInterface *pPin ) const;
+	virtual bool pinShouldAutoRename( fugio::PinInterface *pPin ) const Q_DECL_OVERRIDE;
 
 protected:
 	QSharedPointer<fugio::PinInterface>			 mPinOutputIndex;
 	QSharedPointer<fugio::PinInterface>			 mPinOutputValue;
 
 	fugio::VariantInterface						*mOutputIndex;
+
+	qint64										 mLastUpdate;
 };
 
 #endif // INDEXNODE_H
