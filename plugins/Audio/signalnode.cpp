af11adc60f825b764038ca246b919730d03cfd9a
Alex May <bigfug@gmail.com>
2016-07-31 10:21:52 +0100

    SignalNode clean-up

--- plugins/Audio/signalnode.cpp
+++ plugins/Audio/signalnode.cpp
@@ -86,14 +86,16 @@ bool SignalNode::initialise()
 		return( false );
 	}
 
-	connect( mNode->context()->qobject(), SIGNAL(frameStart(qint64)), this, SLOT(onContextFrame(qint64)) );
+	inputsUpdated( 0 );
+
+	//connect( mNode->context()->qobject(), SIGNAL(frameStart(qint64)), this, SLOT(onContextFrame(qint64)) );
 
 	return( true );
 }
 
 bool SignalNode::deinitialise()
 {
-	disconnect( mNode->context()->qobject(), SIGNAL(frameStart(qint64)), this, SLOT(onContextFrame(qint64)) );
+	//disconnect( mNode->context()->qobject(), SIGNAL(frameStart(qint64)), this, SLOT(onContextFrame(qint64)) );
 
 	return( NodeControlBase::deinitialise() );
 }
