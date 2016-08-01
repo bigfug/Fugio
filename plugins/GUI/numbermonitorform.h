136708a5834bccb6e9b1f989e94b113dd202782f
Alex May <bigfug@gmail.com>
2016-08-01 12:19:53 +0100

    You can save images from NumberMonitorForm by right-clicking on it

--- plugins/GUI/numbermonitorform.h
+++ plugins/GUI/numbermonitorform.h
@@ -23,6 +23,11 @@ public slots:
 protected:
 	virtual void paintEvent( QPaintEvent *pEvent );
 
+	void contextMenuEvent( QContextMenuEvent *pEvent ) Q_DECL_OVERRIDE;
+
+private slots:
+	void saveImage( void );
+
 private:
 	Ui::NumberMonitorForm *ui;
 
@@ -30,6 +35,7 @@ private:
 	int										 mIndex;
 	double									 mMin, mMax;
 	QVector<int>							 mY;
+	QString									 mSaveImageDir;
 };
 
 #endif // NUMBERMONITORFORM_H
