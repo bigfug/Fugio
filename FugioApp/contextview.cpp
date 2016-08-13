14d67f7902869944df6587efdb6b15f7e03b79a2
Alex May <bigfug@gmail.com>
2016-08-13 16:44:54 +0100

    Fixed mouse zooming

--- FugioApp/contextview.cpp
+++ FugioApp/contextview.cpp
@@ -2655,11 +2655,18 @@ void ContextView::wheelEvent( QWheelEvent *pEvent )
 	{
 		if( pEvent->modifiers().testFlag( Qt::ControlModifier ) )
 		{
-			int numSteps = pEvent->delta() / 15 / 8;
+			QPoint numSteps = numDegrees / 15;
 
-			qreal sc = pow( 1.25, numSteps );
+			if( numSteps.y() > 0 )
+			{
+				mScaleFactor += qMax( 0.1, qreal( numSteps.y() ) * 0.1 );
+			}
+			else
+			{
+				mScaleFactor -= qMax( 0.1, qreal( -numSteps.y() ) * 0.1 );
+			}
 
-			zoom( sc, mapToScene( pEvent->pos() ) );
+			zoom( mScaleFactor, mapToScene( pEvent->pos() ) );
 		}
 		else
 		{
