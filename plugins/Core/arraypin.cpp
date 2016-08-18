c94e95b4cb6c5b67fbd126f5cf447016057175bb
Alex May <bigfug@gmail.com>
2016-08-18 17:21:24 +0100

    ArrayPin has its type on its tool tip now

--- plugins/Core/arraypin.cpp
+++ plugins/Core/arraypin.cpp
@@ -11,7 +11,7 @@ ArrayPin::ArrayPin( QSharedPointer<fugio::PinInterface> pPin )
 
 QString ArrayPin::toString() const
 {
-	return( QString( "<p>Count: %1</p><p>Size: %2</p><p>Total Size: %3</p>" ).arg( count() ).arg( size() ).arg( count() * stride() ) );
+	return( QString( "<p>Type: %1</p><p>Count: %2</p><p>Size: %3</p><p>Total Size: %4</p>" ).arg( QString( QMetaType::typeName( type() ) ) ).arg( count() ).arg( size() ).arg( count() * stride() ) );
 }
 
 void ArrayPin::loadSettings( QSettings &pSettings )
