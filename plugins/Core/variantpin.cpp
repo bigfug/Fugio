9d08f10733a0e5cc2852cc835ef17b046e0a6619
Alex May <bigfug@gmail.com>
2017-10-19 17:29:33 +0100

    VariantPin shows data now

--- plugins/Core/variantpin.cpp
+++ plugins/Core/variantpin.cpp
@@ -1,8 +1,36 @@
 #include "variantpin.h"
 
+#include <QPointF>
+#include <QSizeF>
+
 VariantPin::VariantPin( QSharedPointer<fugio::PinInterface> pPin )
 	: PinControlBase( pPin )
 {
 
+}
+
+QString VariantPin::toString() const
+{
+	switch( QMetaType::Type( mValue.type() ) )
+	{
+		case QMetaType::QPointF:
+			{
+				QPointF		V = mValue.value<QPointF>();
+
+				return( QString( "%1,%2 (%3)" ).arg( V.x() ).arg( V.y() ) ).arg( QString( mValue.typeName() ) );
+			}
+
+		case QMetaType::QSizeF:
+			{
+				QSizeF		V = mValue.value<QSizeF>();
+
+				return( QString( "%1,%2 (%3)" ).arg( V.width() ).arg( V.height() ) ).arg( QString( mValue.typeName() ) );
+			}
+
+		default:
+			break;
+	}
+
+	return( QString( "%1 (%2)" ).arg( mValue.toString() ).arg( QString( mValue.typeName() ) ) );
 }
 
