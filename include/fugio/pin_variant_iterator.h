5f65f14b1bfeca3867e5be3e994dbbe1681ba056
Alex May <bigfug@gmail.com>
2017-12-08 11:32:05 +0000

    Added a PinVariantIterator helper class

---
+++ include/fugio/pin_variant_iterator.h
@@ -0,0 +1,85 @@
+#ifndef PIN_VARIANT_ITERATOR_H
+#define PIN_VARIANT_ITERATOR_H
+
+#include <QSharedPointer>
+
+#include <fugio/core/variant_interface.h>
+#include <fugio/core/list_interface.h>
+#include <fugio/pin_interface.h>
+
+FUGIO_NAMESPACE_BEGIN
+
+class PinVariantIterator
+{
+public:
+	PinVariantIterator( const QSharedPointer<fugio::PinInterface> &pPin )
+		: mVariant( nullptr ), mList( nullptr )
+	{
+		QSharedPointer<fugio::PinInterface>	CP = pPin->connectedPin();
+
+		if( CP && CP->hasControl() )
+		{
+			mVariant = qobject_cast<fugio::VariantInterface *>( CP->control()->qobject() );
+			mList    = qobject_cast<fugio::ListInterface *>( CP->control()->qobject() );
+		}
+
+		if( !mVariant && !mList )
+		{
+			mValue = pPin->value();
+		}
+	}
+
+	int size( void ) const
+	{
+		if( mList )
+		{
+			return( mList->listSize() );
+		}
+
+		if( mVariant || !mValue.isNull() )
+		{
+			return( 1 );
+		}
+
+		return( 0 );
+	}
+
+	QVariant index( int pIndex ) const
+	{
+		if( mList )
+		{
+			return( mList->listIndex( pIndex ) );
+		}
+
+		if( pIndex )
+		{
+			return( QVariant() );
+		}
+
+		return( mVariant ? mVariant->variant() : mValue );
+	}
+
+	bool isEmpty( void ) const
+	{
+		if( mList )
+		{
+			return( mList->listIsEmpty() );
+		}
+
+		if( mVariant || !mValue.isNull() )
+		{
+			return( false );
+		}
+
+		return( true );
+	}
+
+protected:
+	fugio::VariantInterface		*mVariant;
+	fugio::ListInterface		*mList;
+	QVariant					 mValue;
+};
+
+FUGIO_NAMESPACE_END
+
+#endif // PIN_VARIANT_ITERATOR_H
