f5baa391077dad82606612a26910106a9c1126b8
Alex May <bigfug@gmail.com>
2018-01-17 23:38:43 +0000

    Added NDI support

---
+++ cmake/FindNDI.cmake
@@ -0,0 +1,28 @@
+# - Try to find NDI
+# Once done, this will define
+#
+#  NDI_FOUND - system has NDI
+#  NDI_INCLUDE_DIRS - the NDI include directories
+#  NDI_LIBRARIES - link these to use NDI
+#  NDI_VERSION - detected version of NDI
+#
+# See documentation on how to write CMake scripts at
+# http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+find_library( NDI_LIBRARY  Processing.NDI.Lib.x86.lib
+  HINTS
+  $ENV{NDI_DIR}
+)
+
+find_path( NDI_INCLUDE_DIR Processing.NDI.Lib.h
+  HINTS
+  $ENV{NDI_DIR}
+)
+
+set( NDI_LIBRARIES ${NDI_LIBRARY} )
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args( NDI REQUIRED_VARS NDI_LIBRARIES NDI_INCLUDE_DIR )
+
+mark_as_advanced( NDI_LIBRARY )
