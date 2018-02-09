bdf034cc958a5b81c587b39c3544865db06dfdc6
Alex May <bigfug@gmail.com>
2018-02-09 12:01:26 +0000

    Disabled AUTO_RCC because it was calling it before processing qm files

--- cmake/AddTranslations.cmake
+++ cmake/AddTranslations.cmake
@@ -8,8 +8,10 @@ qt5_create_translation( QM_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${TS_FILES} )
 
 configure_file( ${CMAKE_SOURCE_DIR}/translations.qrc ${CMAKE_CURRENT_BINARY_DIR} COPYONLY )
 
+qt5_add_resources( QRC_FILES ${CMAKE_CURRENT_BINARY_DIR}/translations.qrc )
+
 target_sources( ${PROJECT_NAME} PRIVATE
 	${TS_FILES}
 	${QM_FILES}
-	${CMAKE_CURRENT_BINARY_DIR}/translations.qrc
+	${QRC_FILES}
 	)
