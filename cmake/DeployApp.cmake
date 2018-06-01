af61be75b025c94c75ffeaca5bfae78fab76f483
Alex May <bigfug@gmail.com>
2018-06-01 14:27:45 +0100

    Finally nailed Windows deployment (for now...)

--- cmake/DeployApp.cmake
+++ cmake/DeployApp.cmake
@@ -47,38 +47,37 @@ if( WIN32 AND CMAKE_BUILD_TYPE STREQUAL Release )
 	get_filename_component( ABS_BINARY_DIR "${CMAKE_INSTALL_PREFIX}" REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 
 	# Run windeployqt immediately after build
-    add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${CMAKE_COMMAND}" -E
-            env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
-                --verbose 0
-                --no-compiler-runtime
-                --no-angle
-                --no-opengl-sw
+	add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+	  COMMAND "${WINDEPLOYQT_EXECUTABLE}"
+		--verbose 2
+		--no-compiler-runtime
+		--no-angle
+		--no-opengl-sw
 				--concurrent --opengl --serialport --websockets --network --qml --quick --quickwidgets
 				--qmldir "${CMAKE_SOURCE_DIR}/qml"
 				--dir "${ABS_BINARY_DIR}/${PATH_APP}"
 				--libdir "${ABS_BINARY_DIR}/${PATH_APP}"
 				--plugindir "${ABS_BINARY_DIR}/${PATH_APP}"
-				\"$<TARGET_FILE:${target}>\"
-    )
+				\"$<TARGET_FILE:${PROJECT_NAME}>\"
+	)
 
-	file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${target}_path"
-		CONTENT "$<TARGET_FILE:${target}>"
+	file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}_$<CONFIG>_path"
+		CONTENT "$<TARGET_FILE:${PROJECT_NAME}>"
 	)
 
 	# Before installation, run a series of commands that copy each of the Qt
 	# runtime files to the appropriate directory for installation
-	install(CODE
+	install( CODE
 		"
-		file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}_path\" _file)
+		file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}_Release_path\" _file)
 		execute_process(
-			COMMAND \"${CMAKE_COMMAND}\" -E
-				env PATH=\"${_qt_bin_dir}\" \"${WINDEPLOYQT_EXECUTABLE}\"
+			COMMAND \"${WINDEPLOYQT_EXECUTABLE}\"
 					--dry-run
 					--no-compiler-runtime
 					--no-angle
 					--no-opengl-sw
 					--list mapping
+					--concurrent --opengl --serialport --websockets --network --qml --quick --quickwidgets
 					--qmldir \"${CMAKE_SOURCE_DIR}/qml\"
 					--dir \"${ABS_BINARY_DIR}/${PATH_APP}\"
 					--libdir \"${ABS_BINARY_DIR}/${PATH_APP}\"
@@ -87,18 +86,34 @@ if( WIN32 AND CMAKE_BUILD_TYPE STREQUAL Release )
 			OUTPUT_VARIABLE _output
 			OUTPUT_STRIP_TRAILING_WHITESPACE
 		)
+
 		separate_arguments(_files WINDOWS_COMMAND \${_output})
+
 		while(_files)
 			list(GET _files 0 _src)
 			list(GET _files 1 _dest)
+			message( \${_src} )
 			execute_process(
 				COMMAND \"${CMAKE_COMMAND}\" -E
-					copy \${_src} \"\${CMAKE_INSTALL_PREFIX}/${directory}/\${_dest}\"
+				copy \${_src} \"\${CMAKE_INSTALL_PREFIX}/${PATH_APP}/\${_dest}\"
 			)
 			list(REMOVE_AT _files 0 1)
 		endwhile()
 		"
 	)
+
+	# windeployqt doesn't work correctly with the system runtime libraries,
+	# so we fall back to one of CMake's own modules for copying them over
+	set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
+	include(InstallRequiredSystemLibraries)
+	foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+		get_filename_component(filename "${lib}" NAME)
+		add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+			COMMAND "${CMAKE_COMMAND}" -E
+				copy_if_different "${lib}" \"$<TARGET_FILE_DIR:${PROJECT_NAME}>\"
+		)
+	endforeach()
+
 endif()
 
 if( APPLE AND CMAKE_BUILD_TYPE STREQUAL Release )
@@ -116,4 +131,3 @@ if( APPLE AND CMAKE_BUILD_TYPE STREQUAL Release )
 	)
 
 endif()
-
