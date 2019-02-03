bcc5534847b0bc6e4ffa13e03234fe0f1557906c
Alex May <bigfug@gmail.com>
2019-02-03 18:13:16 +0000

    Working on UNIX depends

--- cmake/UnixDepends.cmake
+++ cmake/UnixDepends.cmake
@@ -2,6 +2,5 @@ if( UNIX AND NOT APPLE )
 	add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD
 		COMMAND objdump -p ${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_NAME:${PROJECT_NAME}> | grep NEEDED | awk '{ print $2 }' | xargs dpkg -S | awk '{ print $1 }' | cut -d ':' -f1 | sort | uniq > ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.depends
 		BYPRODUCTS ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.depends
-		VERBATIM
 		)
 endif( UNIX AND NOT APPLE )
