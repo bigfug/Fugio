c808a7bd61d46d1fa5345f3410270f9dfc82eadb
Alex May <bigfug@gmail.com>
2024-02-27 10:49:50 +0000

    CMAKE completes under Qt6

--- cmake/AddTranslations.cmake
+++ cmake/AddTranslations.cmake
@@ -5,11 +5,22 @@ set( TS_FILES
 	translations/translations_it.ts
 	)
 
-qt5_create_translation( QM_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${TS_FILES} OPTIONS "-silent" )
+if(COMMAND qt_create_translation)
+        qt_create_translation(QM_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${TS_FILES})
+else()
+        qt5_create_translation(QM_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${TS_FILES})
+endif()
+
+#qt5_create_translation( QM_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${TS_FILES} OPTIONS "-silent" )
 
 configure_file( ${CMAKE_SOURCE_DIR}/translations.qrc ${CMAKE_CURRENT_BINARY_DIR} COPYONLY )
 
-qt5_add_resources( QRC_FILES ${CMAKE_CURRENT_BINARY_DIR}/translations.qrc )
+if(COMMAND qt_add_resources)
+    qt_add_resources( QRC_FILES ${CMAKE_CURRENT_BINARY_DIR}/translations.qrc )
+else()
+    qt5_add_resources( QRC_FILES ${CMAKE_CURRENT_BINARY_DIR}/translations.qrc )
+endif()
+
 
 target_sources( ${PROJECT_NAME} PRIVATE
 	${TS_FILES}
