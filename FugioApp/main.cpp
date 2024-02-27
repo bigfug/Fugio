c64f712bb0bf828cefff9850f0808e6f9cbdad8b
Alex May <bigfug@gmail.com>
2024-02-27 17:15:59 +0000

    Fixed bootstrap url

--- FugioApp/main.cpp
+++ FugioApp/main.cpp
@@ -134,7 +134,7 @@ int main( int argc, char *argv[] )
 			}
 			else if( MsgBox.clickedButton() == buttonYes )
 			{
-				PluginActionDownload    BootstrapDown( QUrl( "https://raw.githubusercontent.com/bigfug/Fugio/bootstrap/fugio-bootstrap.json" ) );
+				PluginActionDownload    BootstrapDown( QUrl( "https://github.com/bigfug/Fugio/releases/download/bootstrap/fugio-bootstrap.json" ) );
 
 				BootstrapDown.setAutoRemove( true );
 
