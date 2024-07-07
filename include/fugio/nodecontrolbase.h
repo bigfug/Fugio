2dd1dd2c64f93698fd5e11e47a505314972c5fd9
Alex May <bigfug@gmail.com>
2024-07-07 18:46:12 +0100

    Updated NodeControlBase with breaking changes

--- include/fugio/nodecontrolbase.h
+++ include/fugio/nodecontrolbase.h
@@ -18,30 +18,38 @@
 
 #define FUGID(x,y) 	static const QUuid x = QUuid( "{" y "}" );
 
-// A section of copy and paste id's for updating legacy code based on PID_UUID
+// A section of copy and paste id's
 
-//FUGID( PIN_XXX_XXX, "9e154e12-bcd8-4ead-95b1-5a59833bcf4e" );
-//FUGID( PIN_XXX_XXX, "1b5e9ce8-acb9-478d-b84b-9288ab3c42f5" );
-//FUGID( PIN_XXX_XXX, "261cc653-d7fa-4c34-a08b-3603e8ae71d5" );
-//FUGID( PIN_XXX_XXX, "249f2932-f483-422f-b811-ab679f006381" );
-//FUGID( PIN_XXX_XXX, "ce8d578e-c5a4-422f-b3c4-a1bdf40facdb" );
-//FUGID( PIN_XXX_XXX, "e6bf944e-5f46-4994-bd51-13c2aa6415b7" );
-//FUGID( PIN_XXX_XXX, "a2bbf374-0dc8-42cb-b85a-6a43b58a348f" );
-//FUGID( PIN_XXX_XXX, "51297977-7b4b-4e08-9dea-89a8add4abe0" );
-//FUGID( PIN_XXX_XXX, "c997473a-2016-466b-9128-beacb99870a2" );
-//FUGID( PIN_XXX_XXX, "e27ce75e-fb9f-4a57-97b8-1c2d966f053b" );
-//FUGID( PIN_XXX_XXX, "3d995a98-0f56-4371-a1fa-67ecaefeba1c" );
-//FUGID( PIN_XXX_XXX, "d77f1053-0f88-4b4a-bfc6-57be3bb1eddd" );
-//FUGID( PIN_XXX_XXX, "1ceaeb2d-0463-44bc-9b80-10129cd8f4eb" );
-//FUGID( PIN_XXX_XXX, "5064e449-8b0b-4447-9009-c81997f754ef" );
-//FUGID( PIN_XXX_XXX, "5c8f8f4e-58ce-4e47-9e1e-4168d17e1863" );
-//FUGID( PIN_XXX_XXX, "a9b8d8d8-e4aa-4a99-b4eb-ea22f1945c63" );
-//FUGID( PIN_XXX_XXX, "da55f50c-1dee-47c9-bee5-6a2e48672a13" );
-//FUGID( PIN_XXX_XXX, "0fb3ba87-ff71-41bc-84ee-4f488a18068f" );
-//FUGID( PIN_XXX_XXX, "b13398e2-4e71-460f-a3b1-499ce9af224d" );
-//FUGID( PIN_XXX_XXX, "fe19f444-7a5f-47e6-ae83-ceb43b8ae915" );
+/*
+FUGID( PIN_XXX_XXX, "9e154e12-bcd8-4ead-95b1-5a59833bcf4e" );
+FUGID( PIN_XXX_XXX, "1b5e9ce8-acb9-478d-b84b-9288ab3c42f5" );
+FUGID( PIN_XXX_XXX, "261cc653-d7fa-4c34-a08b-3603e8ae71d5" );
+FUGID( PIN_XXX_XXX, "249f2932-f483-422f-b811-ab679f006381" );
+FUGID( PIN_XXX_XXX, "ce8d578e-c5a4-422f-b3c4-a1bdf40facdb" );
+FUGID( PIN_XXX_XXX, "e6bf944e-5f46-4994-bd51-13c2aa6415b7" );
+FUGID( PIN_XXX_XXX, "a2bbf374-0dc8-42cb-b85a-6a43b58a348f" );
+FUGID( PIN_XXX_XXX, "51297977-7b4b-4e08-9dea-89a8add4abe0" );
+FUGID( PIN_XXX_XXX, "c997473a-2016-466b-9128-beacb99870a2" );
+FUGID( PIN_XXX_XXX, "e27ce75e-fb9f-4a57-97b8-1c2d966f053b" );
+FUGID( PIN_XXX_XXX, "3d995a98-0f56-4371-a1fa-67ecaefeba1c" );
+FUGID( PIN_XXX_XXX, "d77f1053-0f88-4b4a-bfc6-57be3bb1eddd" );
+FUGID( PIN_XXX_XXX, "1ceaeb2d-0463-44bc-9b80-10129cd8f4eb" );
+FUGID( PIN_XXX_XXX, "5064e449-8b0b-4447-9009-c81997f754ef" );
+FUGID( PIN_XXX_XXX, "5c8f8f4e-58ce-4e47-9e1e-4168d17e1863" );
+FUGID( PIN_XXX_XXX, "a9b8d8d8-e4aa-4a99-b4eb-ea22f1945c63" );
+FUGID( PIN_XXX_XXX, "da55f50c-1dee-47c9-bee5-6a2e48672a13" );
+FUGID( PIN_XXX_XXX, "0fb3ba87-ff71-41bc-84ee-4f488a18068f" );
+FUGID( PIN_XXX_XXX, "b13398e2-4e71-460f-a3b1-499ce9af224d" );
+FUGID( PIN_XXX_XXX, "fe19f444-7a5f-47e6-ae83-ceb43b8ae915" );
+*/
 
 FUGIO_NAMESPACE_BEGIN
+
+template <typename T> struct PinEnt
+{
+	QSharedPointer<fugio::PinInterface>			 mPin;
+	T											*mEnt;
+};
 
 class NodeControlBase : public QObject, public fugio::NodeControlInterface
 {
@@ -50,33 +58,8 @@ class NodeControlBase : public QObject, public fugio::NodeControlInterface
 
 public:
 	explicit NodeControlBase( QSharedPointer<fugio::NodeInterface> pNode )
-		: mNode( pNode ), mPidIdx( 0 ), mInitialisedCalled( false ), mDeinitialisedCalled( false ), mIsPairing( false )
+		: mNode( pNode ), mInitialisedCalled( false ), mDeinitialisedCalled( false ), mIsPairing( false )
 	{
-		// this will be going soon
-
-		if( PID_UUID.isEmpty() )
-		{
-			PID_UUID.append( QUuid( "{9e154e12-bcd8-4ead-95b1-5a59833bcf4e}" ) );
-			PID_UUID.append( QUuid( "{1b5e9ce8-acb9-478d-b84b-9288ab3c42f5}" ) );
-			PID_UUID.append( QUuid( "{261cc653-d7fa-4c34-a08b-3603e8ae71d5}" ) );
-			PID_UUID.append( QUuid( "{249f2932-f483-422f-b811-ab679f006381}" ) );
-			PID_UUID.append( QUuid( "{ce8d578e-c5a4-422f-b3c4-a1bdf40facdb}" ) );
-			PID_UUID.append( QUuid( "{e6bf944e-5f46-4994-bd51-13c2aa6415b7}" ) );
-			PID_UUID.append( QUuid( "{a2bbf374-0dc8-42cb-b85a-6a43b58a348f}" ) );
-			PID_UUID.append( QUuid( "{51297977-7b4b-4e08-9dea-89a8add4abe0}" ) );
-			PID_UUID.append( QUuid( "{c997473a-2016-466b-9128-beacb99870a2}" ) );
-			PID_UUID.append( QUuid( "{e27ce75e-fb9f-4a57-97b8-1c2d966f053b}" ) );
-			PID_UUID.append( QUuid( "{3d995a98-0f56-4371-a1fa-67ecaefeba1c}" ) );
-			PID_UUID.append( QUuid( "{d77f1053-0f88-4b4a-bfc6-57be3bb1eddd}" ) );
-			PID_UUID.append( QUuid( "{1ceaeb2d-0463-44bc-9b80-10129cd8f4eb}" ) );
-			PID_UUID.append( QUuid( "{5064e449-8b0b-4447-9009-c81997f754ef}" ) );
-			PID_UUID.append( QUuid( "{5c8f8f4e-58ce-4e47-9e1e-4168d17e1863}" ) );
-			PID_UUID.append( QUuid( "{a9b8d8d8-e4aa-4a99-b4eb-ea22f1945c63}" ) );
-			PID_UUID.append( QUuid( "{da55f50c-1dee-47c9-bee5-6a2e48672a13}" ) );
-			PID_UUID.append( QUuid( "{0fb3ba87-ff71-41bc-84ee-4f488a18068f}" ) );
-			PID_UUID.append( QUuid( "{b13398e2-4e71-460f-a3b1-499ce9af224d}" ) );
-			PID_UUID.append( QUuid( "{fe19f444-7a5f-47e6-ae83-ceb43b8ae915}" ) );
-		}
 	}
 
 	virtual ~NodeControlBase( void ) Q_DECL_OVERRIDE {}
@@ -224,28 +207,37 @@ public:
 
 	// dont use this version in new code
 
-	QSharedPointer<fugio::PinInterface> pinInput( const QString &pName ) // soon... Q_DECL_DEPRECATED
-	{
-		return( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), next_uuid() ) );
-	}
+	// QSharedPointer<fugio::PinInterface> pinInput( const QString &pName ) // soon... Q_DECL_DEPRECATED
+	// {
+	// 	return( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), next_uuid() ) );
+	// }
 
 	QSharedPointer<fugio::PinInterface> pinInput( const QString &pName, const QUuid &pLocalId )
 	{
 		return( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), pLocalId ) );
 	}
 
-	template <class T> T pinInput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID ) // soon... Q_DECL_DEPRECATED
-	{
-		Q_ASSERT( mPinInterface.isNull() );
+	// template <class T> T pinInput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID ) // soon... Q_DECL_DEPRECATED
+	// {
+	// 	Q_ASSERT( mPinInterface.isNull() );
 
-		return( qobject_cast<T>( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), next_uuid(), mPinInterface, pControlUUID ) ) );
-	}
+	// 	return( qobject_cast<T>( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), next_uuid(), mPinInterface, pControlUUID ) ) );
+	// }
 
 	template <class T = fugio::PinInterface *> T pinInput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID, const QUuid &pLocalId )
 	{
 		Q_ASSERT( mPinInterface.isNull() );
 
 		return( qobject_cast<T>( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), pLocalId, mPinInterface, pControlUUID ) ) );
+	}
+
+	template <class T> bool pinInput( const QString &pName, PinEnt<T> &pPinEnt, const QUuid &pControlUUID, const QUuid &pLocalId )
+	{
+		Q_ASSERT( pPinEnt.mPin.isNull() );
+
+		pPinEnt.mEnt = qobject_cast<T>( mNode->createPin( pName, PIN_INPUT, QUuid::createUuid(), pLocalId, pPinEnt.mPin, pControlUUID ) );
+
+		return( pPinEnt.mEnt != Q_NULLPTR );
 	}
 
 	QSharedPointer<fugio::PinInterface> createProperty( const QString &pName, const QUuid &pLocalId )
@@ -258,6 +250,15 @@ public:
 		Q_ASSERT( mPinInterface.isNull() );
 
 		return( qobject_cast<T>( mNode->createProperty( pName, PIN_INPUT, QUuid::createUuid(), pLocalId, mPinInterface, pControlUUID ) ) );
+	}
+
+	template <class T> bool createProperty( const QString &pName, PinEnt<T> &pPinEnt, const QUuid &pControlUUID, const QUuid &pLocalId )
+	{
+		Q_ASSERT( pPinEnt.mPin.isNull() );
+
+		pPinEnt.mEnt = qobject_cast<T>( mNode->createProperty( pName, PIN_INPUT, QUuid::createUuid(), pLocalId, pPinEnt.mPin, pControlUUID ) );
+
+		return( pPinEnt.mEnt != Q_NULLPTR );
 	}
 
 	//-------------------------------------------------------------------------
@@ -281,18 +282,27 @@ public:
 		return( PinInt );
 	}
 
-	template <class T> T pinOutput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID ) // soon... Q_DECL_DEPRECATED
-	{
-		Q_ASSERT( mPinInterface.isNull() );
+	// template <class T> T pinOutput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID ) // soon... Q_DECL_DEPRECATED
+	// {
+	// 	Q_ASSERT( mPinInterface.isNull() );
 
-		return( qobject_cast<T>( mNode->createPin( pName, PIN_OUTPUT, QUuid::createUuid(), next_uuid(), mPinInterface, pControlUUID ) ) );
-	}
+	// 	return( qobject_cast<T>( mNode->createPin( pName, PIN_OUTPUT, QUuid::createUuid(), next_uuid(), mPinInterface, pControlUUID ) ) );
+	// }
 
 	template <class T> T pinOutput( const QString &pName, QSharedPointer<fugio::PinInterface> &mPinInterface, const QUuid &pControlUUID, const QUuid &pLocalId )
 	{
 		Q_ASSERT( mPinInterface.isNull() );
 
 		return( qobject_cast<T>( mNode->createPin( pName, PIN_OUTPUT, QUuid::createUuid(), pLocalId, mPinInterface, pControlUUID ) ) );
+	}
+
+	template <class T> bool pinOutput( const QString &pName, PinEnt<T> &pPinEnt, const QUuid &pControlUUID, const QUuid &pLocalId )
+	{
+		Q_ASSERT( pPinEnt.mPin.isNull() );
+
+		pPinEnt.mEnt = qobject_cast<T *>( mNode->createPin( pName, PIN_OUTPUT, QUuid::createUuid(), pLocalId, pPinEnt.mPin, pControlUUID ) );
+
+		return( pPinEnt.mEnt != Q_NULLPTR );
 	}
 
 	//-------------------------------------------------------------------------
@@ -592,23 +602,8 @@ protected:
 		}
 	}
 
-	//-------------------------------------------------------------------------
-	// don't use this in new code!
-
-	QUuid next_uuid( void ) // soon... Q_DECL_DEPRECATED
-	{
-		Q_ASSERT( mPidIdx < PID_UUID.size() );
-
-		return( PID_UUID[ mPidIdx++ ] );
-	}
-
 protected:
 	QSharedPointer<fugio::NodeInterface>	 mNode;
-
-	// don't use this in new code
-
-	int										 mPidIdx;
-	static QList<QUuid>						 PID_UUID;
 
 	Options									 mOptions;
 
